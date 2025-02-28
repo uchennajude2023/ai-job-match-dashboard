29e8d101d471074487e22d5d32efa335caf4018b
Uchenna Jude <emmauchenna1991@gmail.com>
2025-02-28 21:17:19 +0100

    Fixed ESLint errors

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
-  swcMinify: true, // Enables faster builds
   eslint: {
     ignoreDuringBuilds: true, // Prevents ESLint errors from stopping deployment
   }
