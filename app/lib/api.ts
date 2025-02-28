5a0ad2b6e29d262e2ad4330bec1ee8556970f480
Uchenna Jude <emmauchenna1991@gmail.com>
2025-02-28 23:53:25 +0100

    added external api

--- app/lib/api.ts
+++ app/lib/api.ts
@@ -1,6 +1,6 @@
-// src/utils/api.ts
+
 export const fetchJobDetails = async (id: string) => {
-    const response = await fetch(`http://localhost:5000/jobs/${id}`);
+    const response = await fetch(`https://67c239b961d8935867e5b2c6.mockapi.io/api/v1/jobs/${id}`);
     if (!response.ok) throw new Error("Failed to fetch job details");
     return response.json();
   };
